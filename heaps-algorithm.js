17dbe03e0f033c555bae6561b38a707146facc07
Luis Angel Maldonado <lmaldonadoch@gmail.com>
2020-08-17 09:09:59 -0500

    Add Heap's Algorithm

---
+++ heaps-algorithm.js
@@ -0,0 +1,29 @@
+const getPermutations = (arr) => {
+  const result = [];
+
+  const swapInPlace = (arrToSwap, indexA, indexB) => {
+    const temp = arrToSwap[indexA];
+    arrToSwap[indexA] = arrToSwap[indexB];
+    arrToSwap[indexB] = temp;
+  };
+
+  const generate = (n, heapArr) => {
+    if (n === 1) {
+      result.push(heapArr.slice());
+      return;
+    }
+
+    generate(n - 1, heapArr);
+
+    for (let i = 0; i < n - 1; i++) {
+      if (n % 2 === 0) {
+        swapInPlace(heapArr, i, n - 1);
+      } else {
+        swapInPlace(heapArr, 0, n - 1);
+      }
+      generate(n - 1, heapArr);
+    }
+  };
+
+  return result;
+};
